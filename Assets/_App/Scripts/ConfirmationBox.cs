a310875ab230e365851351c7a80b8e6ea7fc697c
Khin Baptista <kerbaptista@inf.ufrgs.br>
2015-11-11 22:11:06 -0200

    Changed to Unity version 32 bits + created confirmation box popup (not sure if it is finished, probably not)

---
+++ Assets/_App/Scripts/ConfirmationBox.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ConfirmationBox : MonoBehaviour {
+
+	public delegate void Callback();
+
+	public Callback OnConfirm;
+	public Callback OnCancel;
+	private Callback OnAnimationEnd;
+
+	[SerializeField]
+	private Text text;
+	private Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+
+		if (anim == null)
+			enabled = false;
+	}
+	
+	void Update () {
+	
+	}
+
+	public void Show(string message) {
+		text.text = message;
+		anim.SetBool("display", true);
+	}
+	
+	public void OnConfirmClick() {
+		OnAnimationEnd = OnConfirm;
+		anim.SetBool("display", false);
+	}
+
+	public void OnCancelClick() {
+		OnAnimationEnd = OnCancel;
+		anim.SetBool("display", false);
+	}
+
+	public void AnimationFinished() {
+		OnAnimationEnd();
+	}
+}
