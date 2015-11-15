fc938d81d3daa2fed7f40afbd0973c2a2d88eb3e
Khin Baptista <kerbaptista@inf.ufrgs.br>
2015-11-15 19:32:35 -0200

    Created scene to test vuforia stuff + script to control AR elements (tracker, builder, and so on)

---
+++ Assets/_App/Scripts/AR/AREventHandler.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Vuforia;
+
+public class AREventHandler : MonoBehaviour, IUserDefinedTargetEventHandler
+{
+	[SerializeField]
+	private string targetName = "Image Target";
+	[SerializeField]
+	private ImageTargetBehaviour _targetTemplate;
+
+	private UserDefinedTargetBuildingBehaviour _targetBuilder;
+	private ObjectTracker _objectTracker;
+	private DataSet _dataSet;
+	private ImageTargetBuilder.FrameQuality _frameQuality = ImageTargetBuilder.FrameQuality.FRAME_QUALITY_NONE;
+
+
+	void Start () {
+		_targetBuilder = GetComponent<UserDefinedTargetBuildingBehaviour>();
+
+		if (_targetBuilder != null)
+			_targetBuilder.RegisterEventHandler(this);
+	}
+	
+	void Update () {
+	
+	}
+
+	public void OnInitialized() {
+		_objectTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+
+		if (_objectTracker != null) {
+			_dataSet = _objectTracker.CreateDataSet();
+			_objectTracker.ActivateDataSet(_dataSet);
+		}
+	}
+
+	public void OnFrameQualityChanged(ImageTargetBuilder.FrameQuality frameQuality) {
+		_frameQuality = frameQuality;
+	}
+
+	public void OnNewTrackableSource(TrackableSource trackableSource) {
+		_objectTracker.DeactivateDataSet(_dataSet);
+
+		_dataSet.DestroyAllTrackables(true);
+
+		ImageTargetBehaviour targetCopy = (ImageTargetBehaviour)Instantiate(_targetTemplate);
+		targetCopy.gameObject.name = targetName;
+
+		_dataSet.CreateTrackable(trackableSource, targetCopy.gameObject);
+		_objectTracker.ActivateDataSet(_dataSet);
+	}
+
+	public void AddNewTrackableSource() {
+		_targetBuilder.BuildNewTarget(targetName, _targetTemplate.GetSize().x);
+	}
+}
